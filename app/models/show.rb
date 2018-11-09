99f065c4e025bf161c196e997518daf6bdea7141
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-09 02:20:51 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,6 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show 
-      max= Show.maximum(:rating)
-      Show.where(:rating  max)
+      Show.where(:rating Show.maximum(:rating))
   end 
 end 
